39e3532fef631a3b24b926790f213deec060483e
inouetogo <83203336+inouetogo@users.noreply.github.com>
2021-07-19 16:10:54 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-var url = "news.rof";
+var url = "news.rdf";
+
+$.get(url).done(function(result){
+    var ret = $('item',result);
+    console.log(ret);
+
+    for( var i = 0; i < 3 ; ++i){
+        var item = ret[i];
+        var $card = $("#col" + (i +1));
+        $card.find(".card-img-top").attr("src" , $('enclosure', item).attr("url") )
+        $card.find(".card-title").text( $('title',item).text())
+    }
+
+}).fail(function(result){
+    console.log(result);
+});
